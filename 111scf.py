5384c5de6d37c9c14eb165c2927e44e574a62cba
jul <jul018@ucsd.edu>
2020-06-07 01:44:30 +0000

    code

--- 111scf.py
+++ 111scf.py
@@ -3,20 +3,20 @@ import shutil
 import numpy as np
 
 # Load the Si.pw.in.template file as a template.
-with open("Al.111.H4.et0.template") as f:
+with open("Al.111.H3.et1.template") as f:
     template = f.read()
 
 # Set default values for various parameters
-i=0
+i=1
 
 # Loop through different k-points.
-for i in np.arange(0, 10, 10):
+for i in np.arange(1, 10, 10):
     # This generates a string from the template with the parameters replaced
     # by the specified values.
     # s = template.format(k1=k1, k3=k3, alat=alat, ecut=ecut)
 
     # Let's define an easy jobname.
-    jobname = "Al_111_H4_hole_A_site_%s" % (i)
+    jobname = "Al_111_H3_hole_B_site_%s" % (i)
 
     # Write the actual input file for PWSCF.
     with open("%s.pw.in" % jobname, "w") as f:
